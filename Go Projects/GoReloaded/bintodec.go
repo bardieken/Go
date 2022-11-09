1b09f4dfd8865c931f8a62ac71a0c90ab792f880
bardieken <brianken891@gmail.com>
2022-11-09 15:24:38 +0200

    c1

--- Go Projects/GoReloaded/bintodec.go
+++ Go Projects/GoReloaded/bintodec.go
@@ -17,3 +17,12 @@ func main() {
 
 	fmt.Printf("Output %d", output)
 }
+
+/* This function converts binary to decimal
+func binToDec(binString string) string {
+	output, err := strconv.ParseInt(binString, 2, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strconv.Itoa(int(output))
+*/
