67fa061f8b05ef502ebf1926df3f97ffce064f0f
bardieken <brianken891@gmail.com>
2022-11-19 00:24:42 +0200

    if

---
+++ Go trainings/go-if/if1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sum int
+	i := 1
+	for {
+		if i > 5 {
+			break
+		}
+		sum += i
+		fmt.Println(i, "--->", sum)
+		i++
+	}
+	fmt.Println("sum =", sum)
+}
