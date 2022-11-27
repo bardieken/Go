acba98951cc673ec1ac6534e53b666259176cba7
bardieken <brianken981@gmail.com>
2022-11-27 23:12:55 +0200

    j

--- GoTrainings/Bootcamp/loopex2.go
+++ GoTrainings/Bootcamp/loopex2.go
@@ -4,54 +4,75 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+)
+
+const (
+	validOps = "%*/+-"
+
+	usageMsg       = "Usage: [op=" + validOps + "] [size]"
+	sizeMissingMsg = "Size is missing"
+	invalidOpMsg   = `Invalid operator.
+Valid ops one of: ` + validOps
+
+	invalidOp = -1
 )
 
 func main() {
-	args := os.Args
-	if len(args) != 3 {
-		fmt.Println("Usage: [op=*/+-] [size]")
+	args := os.Args[1:]
+
+	switch l := len(args); {
+	case l == 1:
+		fmt.Println(sizeMissingMsg)
+		fallthrough
+	case l < 1:
+		fmt.Println(usageMsg)
 		return
 	}
-	op := args[1]
-	size, err := strconv.Atoi(args[2])
-	if err != nil {
-		fmt.Println("Size is not a number")
+
+	op := args[0]
+	if strings.IndexAny(op, validOps) == invalidOp {
+		fmt.Println(invalidOpMsg)
 		return
 	}
-	switch op {
-	case "*":
-		if size < 0 {
-			fmt.Println("Size must be positive")
-			return
-		}
-		fmt.Printf("%5s", "X")
-		for i := 0; i <= size; i++ {
-			fmt.Printf("%5d", i)
+
+	size, err := strconv.Atoi(args[1])
+	if err != nil || size <= 0 {
+		fmt.Println("Wrong size")
+		return
+	}
+
+	fmt.Printf("%5s", op)
+	for i := 0; i <= size; i++ {
+		fmt.Printf("%5d", i)
+	}
+	fmt.Println()
+
+	for i := 0; i <= size; i++ {
+		fmt.Printf("%5d", i)
+
+		for j := 0; j <= size; j++ {
+			var res int
+
+			switch op {
+			case "*":
+				res = i * j
+			case "%":
+				if j != 0 {
+					res = i % j
+				}
+			case "/":
+				if j != 0 {
+					res = i / j
+				}
+			case "+":
+				res = i + j
+			case "-":
+				res = i - j
+			}
+
+			fmt.Printf("%5d", res)
 		}
 		fmt.Println()
-		for i := 0; i <= size; i++ {
-			fmt.Printf("%5d", i)
-			for j := 0; j <= size; j++ {
-				fmt.Printf("%5d", i*j)
-			}
-			fmt.Println()
-		}
-	case "/":
-		if size < 0 {
-			fmt.Println("Size must be positive")
-			return
-		}
-		fmt.Printf("%5s", "X")
-		for i := 1; i <= size; i++ {
-			fmt.Printf("%5d", i)
-		}
-		fmt.Println()
-		for i := 1; i <= size; i++ {
-			fmt.Printf("%5d", i)
-			for j := 1; j <= size; j++ {
-				fmt.Printf("%5d", i/j)
-			}
-			fmt.Println()
-		}
 	}
 }
