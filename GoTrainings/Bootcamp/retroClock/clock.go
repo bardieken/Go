21fd21985f18a378f7a8410b0871948fd23c544e
alpbal <logonme@gmail.com>
2022-12-22 00:23:27 +0200

    gg

---
+++ GoTrainings/Bootcamp/retroClock/clock.go
@@ -0,0 +1,105 @@
+package main
+
+import "fmt"
+
+func main() {
+	type placeholder [5]string
+
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+	digits := []placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+	//for line := range digits[0] {
+	// Print a line for each placeholder in digits
+	//fmt.Println(line)
+	//for digit := range digits {
+	//fmt.Print(digits[digit][line], "  ")
+	//}
+	//fmt.Println()
+	//}
+
+	for i := 0; i < 5; i++ {
+		for _, digit := range digits {
+			fmt.Printf("%s  ", digit[i])
+		}
+		fmt.Println()
+	}
+}
