c879f5f3149e79c506d6244d2f6374624ca458ab
bardieken <brianken891@gmail.com>
2023-01-03 15:22:56 +0200

    jj

---
+++ GoTrainings/Bootcamp/Slices/exercises/srtslicev2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	items := []string{
+		"pacman", "mario", "tetris", "doom", "galaga", "frogger",
+		"asteroids", "simcity", "metroid", "defender", "rayman",
+		"tempest", "ultima",
+	}
+
+	fmt.Println("Original:", items)
+
+	mid := len(items) / 2
+	fmt.Println(mid)
+	smid := items[mid-1 : mid+2]
+	fmt.Println(smid)
+
+	// sorting the smid will affect the items
+	// as well. their backing array is the same.
+	sort.Strings(smid)
+	fmt.Println(smid)
+	fmt.Println()
+	fmt.Println("Sorted  :", items)
+}
